204e0912b157cf83ab6725c64c944fd8bb5dab6a
Alexander <alexsander@proxsis.net>
2025-04-01 09:29:40 -0300

    fear: endpoint to get by id

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import express from 'express';
 const app = express();
 import endpoints from './constants.js';
-import findAll from './service/IPCA.js';
+import IPCAService from './service/IPCA.js';
 
 app.get(endpoints.findAll, (req, res) =>{
-    const historicalInflation = findAll();
+    const historicalInflation = IPCAService.findAll();
     res.json(historicalInflation);
 });
 
 app.get(endpoints.findById, (req, res) => {
     const id = req.params.id;
-    
+    const result = IPCAService.findById(id);
+    result ? res.json(result) : res.status(404).json({message: "IPCA not found"});
 });
 
 app.listen(8080, () => console.log('Server started'));
