8fe35a49b1d58318cbb80594c0e629d7869f2fcd
Alexander <alexsander@proxsis.net>
2025-04-01 10:07:28 -0300

    fear: message erros api

--- constants.js
+++ constants.js
@@ -4,4 +4,8 @@ const endpoints = {
     calculate: "/historicoIPCA/calculo",
 };
 
+export const message = {
+    invalidParams: "Params invalid; \nValue must be bigger than zero; \nYear params must be between 2015 and 2023 \nMonths param must be between 1 and 12"
+}
+
 export default endpoints;
