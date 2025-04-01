dd9de4dc40001ff40d9dab782ee229eefcf77a5d
Alexander <alexsander@proxsis.net>
2025-04-01 07:55:46 -0300

    fear: api base structure

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.json({message: 'hello world'});
+});
+
+app.listen(8080, () => console.log('Server started'));
