03587f2d7a29aa6e7996c4b0b43889faa815adaa
Alexander <alexsander@proxsis.net>
2025-04-01 09:09:24 -0300

    fear: historicalIPCA endpoint

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 const app = express();
 import endpoints from './constants.js';
+import findAll from './service/IPCA.js';
 
 app.get(endpoints.findAll, (req, res) =>{
-    res.json({message: 'hello world'});
+    const historicalInflation = findAll();
+    res.json(historicalInflation);
 });
 
 app.listen(8080, () => console.log('Server started'));
